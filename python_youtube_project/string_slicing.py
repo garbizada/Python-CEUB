7bb20471ee6694998177e41f9fe92d255e6860e7
Cauê Justen Garbi <garbizada@users.noreply.github.com>
2023-06-01 11:48:32 -0300

    Update!

--- python_youtube_project/string_slicing.py
+++ python_youtube_project/string_slicing.py
@@ -3,13 +3,22 @@
 #                   [start:stop:step]
 
 
-name = "Cauê Justen Garbi"
+#name = "Cauê Justen Garbi"
 
-first_name = name[0:4:1]
-last_name = name[12:17:1]       # 12= where it started to show
+#first_name = name[0:4:1]
+#last_name = name[12:17:1]       # 12= where it started to show
                                 # 17 = where it ended
                                 # 1 = how much spaces it goes by
 
-reversed_name = name[::-1]
+#reversed_name = name[::-1]
 
-print(reversed_name)
+#print(reversed_name)
+
+
+website1 = "http://google.com"
+website2 = "http://wikipedia.com"
+
+slice = slice(7,-4)
+
+print(website1[slice])
+print(website2[slice])
