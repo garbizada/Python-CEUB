02bc15ce1888058b6467751582dd196256952144
Cauê Justen Garbi <garbizada@users.noreply.github.com>
2023-06-09 13:48:48 -0300

    studying!!

---
+++ python_youtube_project/lists.py
@@ -0,0 +1,18 @@
+# list = used to store multiple items in a single variable
+
+food = ["pizza","hamburguer", "hotdog","spaghetti"]    # the count starts at 0 so pizza = 0, hamburguer = 1 ....
+
+food[0] = "sushi"
+
+#print(food[0])
+
+#food.append("ice cream")
+#food.pop()
+#food.remove("hotdog")
+#food.insert(0, "cake")
+#food.sort()
+#food.clear()
+
+
+for x in food:
+  print(x)
---
+++ python_youtube_project/twod_lists.py
@@ -0,0 +1,9 @@
+# 2D lists = a list of lists
+
+drinks = ["coffe","soda","tea"]
+dinner = ["pizza","hamburguer","hotdog"]
+dessert = ["cake","ice cream"]
+
+food = [drinks,dinner,dessert]
+
+print(food[2][1])
