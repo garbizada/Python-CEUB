dc23a303757e06343b7ffb5409ae293131bb4327
Cauê Justen Garbi <garbizada@users.noreply.github.com>
2023-09-26 17:13:14 -0300

    trying new librarys such as (panda, pyautogui, etc..)

--- python_hashtag_youtube/codigo.py
+++ python_hashtag_youtube/codigo.py
@@ -1,16 +1,29 @@
 import pyautogui
-
+import pandas
 import time
 
 pyautogui.PAUSE = 0.8
 
-pyautogui.press("win")
-pyautogui.write("opera")
-pyautogui.press("enter")
+#pyautogui.press("win")
+#pyautogui.write("opera")
+#pyautogui.press("enter")
 
 
-link = "https://www.youtube.com/watch?v=BxKahT39AGA"
-pyautogui.write(link)
-pyautogui.press("enter")
+#link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
+#pyautogui.write(link)
+#pyautogui.press("enter")
 
-time.sleep(3)
+#time.sleep(3)
+
+#pyautogui.click(x=818, y=354)
+#pyautogui.press("enter")
+#pyautogui.write("ahoba@gmail.com")
+#pyautogui.press("tab")
+#pyautogui.write("senha senha 123")
+#pyautogui.press("tab")
+
+#time.sleep(3)
+
+#Importando a base de dados de produto
+tabela = pandas.read_csv("python_hashtag_youtube\produtos.csv")
+print(tabela)
